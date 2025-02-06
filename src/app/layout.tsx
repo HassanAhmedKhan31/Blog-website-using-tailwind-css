f2f3fa85e5c96aaf35034f947951187163d7a152
Hassan Ahmed Khan <161369304+HassanAhmedKhan31@users.noreply.github.com>
2025-02-06 05:26:44 -0800

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ThemeProvider } from "@/components/theme-provider";
 import Footer from "@/components/footer";
 import Email from "@/components/email";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -39,7 +26,7 @@ export default function RootLayout({
         >
           <Header />
           {children}
-          <Email/>
+          <Email />
           <Footer />
         </ThemeProvider>
       </body>
